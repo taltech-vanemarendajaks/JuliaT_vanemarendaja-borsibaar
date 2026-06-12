ff6048945915a5097773b2fe9770e5d44c4e1ee8
Julia Taro <julia.taro@gmail.com>
2026-06-12 17:39:25 +0300

    Products by Category size  change

--- frontend/app/(protected)/client/page.tsx
+++ frontend/app/(protected)/client/page.tsx
@@ -119,9 +119,9 @@ export default function ClientProductsByCategory() {
 
         <section className="rounded-2xl bg-[#1b1830] border border-[#2a2640] p-4 flex flex-col basis-2/3">
           <div className="mb-3 flex items-center justify-between">
-            <h2 className="text-lg md:text-xl font-semibold tracking-wide text-[#a7a3c7]">
+            <h3 className="text-lg md:text-xl items-center font-semibold tracking-wide text-[#a7a3c7]">
               Products by Category
-            </h2>
+            </h3>
           </div>
 
           <div
